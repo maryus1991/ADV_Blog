61cad31926aab65405ea2a7ead296187316f16c6
maryus1991 <maryus19915123@gmail.com>
2024-10-23 18:29:34 +0330

    try to fix comment problem

--- blog/api/v1/permissions.py
+++ blog/api/v1/permissions.py
@@ -6,4 +6,13 @@ class IsOwnerOrReadOnly(BasePermission):
     for return if user create this post or not for edit
     """
     def has_object_permission(self, request, view, obj):
-        return object.author.user == request.user
+
+        
+        SAFE_METHODS = ('GET')
+
+        return bool(
+            request.method in SAFE_METHODS or
+            (obj.author == request.user)
+        )
+
+        
--- blog/api/v1/serializers.py
+++ blog/api/v1/serializers.py
@@ -2,9 +2,9 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework import status
-
+from django.urls import reverse
 import datetime
-from blog.models import Post
+from blog.models import Post, PostsComment
 
 User = get_user_model()
 
@@ -17,6 +17,18 @@ class UserModelSerializer(serializers.ModelSerializer):
         fields = ('email', 'first_name', 'last_name', 'avatar')
 
 
+class PostsCommentModelSerializer(serializers.ModelSerializer):
+    """
+    comment model serializer 
+    """
+
+    class Meta:
+        model = PostsComment
+        exclude = ['is_active']
+        read_only_fields = ['id']
+
+
+
 class PostModelSerializer(serializers.ModelSerializer):
     """
     create serializer for posts model 
@@ -27,7 +39,39 @@ class PostModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         exclude = ['is_active']
-        
+        read_only_fields = ["id", "author"]
+    
+    def to_representation(self, instance):
+        """
+        for edit and set post url and set the comment for the post
+        """
+
+        # get the request and representation 
+        request = self.context.get("request")
+        representation = super().to_representation(instance)
+
+        # if the user see the retrieve object
+        if request.parser_context.get("kwargs").get("pk"):
+            
+            # declare comment_dict and get parent_comments that there parent is None
+            comment_dict = {}
+            parent_comments =  PostsComment.objects.filter(post_id=instance.id, parent=None).prefetch_related('child').all()
+
+            # serialize the data
+            parent_comments = PostsCommentModelSerializer(parent_comments, context={'request': request})
+
+            # for set the sub comment for there parent 
+            for comment in parent_comments:
+                comment_dict[comment] = [sub_comment for sub_comment in comment.child.all()]
+
+            # send the serialized data of comments with representation
+            representation['comments'] = comment_dict
+        else:
+            # if user see the list view he will got the post link for see the detail of the post
+            representation['post_link'] = reverse('post-detail', kwargs={'pk': instance.id})
+            
+
+        return representation
     
     def create(self, validated_data):
 
