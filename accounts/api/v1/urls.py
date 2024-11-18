6e0da12f7b2809a510733846a2ab8028b5a8ff41
maryus1991 <maryus19915123@gmail.com>
2024-11-18 16:45:55 +0330

    starting api testing

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -21,21 +21,21 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     # user registrations
-    path("registration", Registrations.as_view()),
-    path("authenticated-user-information", AuthenticatedUserInformation.as_view()),
+    path("registration", Registrations.as_view(), name='Registrations_API'),
+    path("authenticated-user-information", AuthenticatedUserInformation.as_view(), name='AuthenticatedUserInformation_API'),
     # user showing and edit account
-    path("user/<int:pk>", UserInformationShowing.as_view()),
-    path("user/<int:pk>/change-password", UserChangePassWord.as_view()),
-    path("user/<int:pk>/change-email", UserChangeEmail.as_view()),
-    path("user/<int:pk>/update-profile", UserChangeProfile.as_view()),
+    path("user/<int:pk>", UserInformationShowing.as_view(), name='UserInformationShowing_API'),
+    path("user/<int:pk>/change-password", UserChangePassWord.as_view(), name='UserChangePassWord_API'),
+    path("user/<int:pk>/change-email", UserChangeEmail.as_view(), name='UserChangeEmail_API'),
+    path("user/<int:pk>/update-profile", UserChangeProfile.as_view(), name='UserChangeProfile_API'),
     # token authentications urls
-    path("token/login/", CustomAuthenticationsToken.as_view()),
-    path("token/logout/", CustomLogoutToken.as_view()),
+    path("token/login/", CustomAuthenticationsToken.as_view(), name='CustomAuthenticationsToken_API'),
+    path("token/logout/", CustomLogoutToken.as_view(), name='CustomLogoutToken_API'),
     # JWT authentications urls
-    path("jwt/create/", CustomTokenObtainPairView.as_view(), name='CustomTokenObtainPairView'),
-    path("jwt/refresh/", TokenRefreshView.as_view()),
-    path("jwt/verify/", TokenVerifyView.as_view()),
+    path("jwt/create/", CustomTokenObtainPairView.as_view(), name='CustomTokenObtainPairView_API'),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name='TokenRefreshView_API'),
+    path("jwt/verify/", TokenVerifyView.as_view(), name='TokenVerifyView_API'),
     # forgot password and resent email for account activation
-    path("resent-email/", ActivationAccount.as_view()),
-    path("forgot-password/", ForgotPassword.as_view()),
+    path("resent-email/", ActivationAccount.as_view(), name='ActivationAccount_API'),
+    path("forgot-password/", ForgotPassword.as_view(), name='ForgotPassword_API'),
 ]
--- accounts/test/test_acounts_authentications_api.py
+++ accounts/test/test_acounts_authentications_api.py
@@ -1,47 +1,76 @@
 from django.urls import reverse
-from django.contrib.auth import get_user_model
+from django.utils.crypto import get_random_string
 from rest_framework.test import APIClient
-import pytest
+from django.test import TestCase
 
-User = get_user_model()
+from accounts.models import User
 
-user = {
+
+user_options = {
     "email": "admin@admin.domain",
     "password": "@Aa1234567890",
 }
 
-@pytest.fixture
-def client():
-    """
-    create and get APIClient with jwt token in headers for auth user that tested for jwt token apis
-    """
-    client = APIClient()
-    User.objects.create_user(
-        email=user.get("email"), password=user.get("password")
-    )
-    response = client.post(
-        reverse('CustomTokenObtainPairView'),
-        {
-            "email": user.get("email"),
-            "password": user.get("password"),
-        },
-    )
-
-    print(response.json().get("access"))
-    headers = {"HTTP_AUTHORIZATION": "Bearer " + response.json().get("access")}
-    assert response.status_code == 201
-    client.credentials(**headers)
-    """
-    the return is about three section in tuple the first is the jwt 
-    token in header client the second is the response for getting
-    jwt tokens and the third is for senf=ding anonymous requests
-    """
-    return (client, response.json(), APIClient())
 
 
-@pytest.mark.django_db
-class TestUserAuthenticationsAPI:
-    '''
-    for test user login, registration, reset email, forgot password, api 
-    '''
-    pass
+class TestAccountAuthenticatedApi(TestCase):
+    
+    def setUP(self):
+
+        # create user  
+        self.user = User.objects.create_user(
+            email = user_options.get('email'),
+            password = user_options.get('password')     
+        ) 
+
+        self.user.is_verified = True
+        self.user.verified_code = get_random_string(255)
+        self.user.save()
+
+        # create anonymous and logged in request
+
+        anonymous_client = APIClient()
+
+        client = APIClient()
+
+
+        # get jwt token and check
+        jwt_url = reverse('CustomTokenObtainPairView_API')
+
+        result = client.post(jwt_url, user_options)
+        self.assertEqual(result.status_code, 200)
+
+        jwt_token = result.json().get('access') 
+
+        # set the header
+        headers = {"HTTP_AUTHORIZATION": "Bearer " + result.json().get("access")}
+        client.credentials(**headers)
+        
+
+
+    def test_registration_api_url_with_valid_and_invalid_data(self):
+
+        # get the url
+        url = reverse('Registrations_API')
+
+        # send request with valid data
+        response = anonymous_client.post(url, {
+            "email": "user@example.com",
+            "first_name": "mostafa",
+            "last_name": "khenifary",
+            "password": "@123456789Aa",
+            "conform_password": "@123456789Aa"
+        })
+
+        self.assertEqual(response.status_code, 201)
+
+        # checking in database
+        self.assertTrue(
+            User.objects.filter(email='user@example.com').exists()
+        )
+
+
+
+
+
+        
